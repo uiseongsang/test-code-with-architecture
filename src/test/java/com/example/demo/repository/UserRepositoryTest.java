a8ba9bfb8ac8f00bfad4a1577131dc407348b72b
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-08-04 00:29:52 +0900

    :sparkles: repository 테스트(최종 - @sql 사용)
    save코드가 중복이 되어서 삭제해주고
    테스트 값을 미리 준비해주고
    테스트할 떄는 준비된 값을 사용

--- src/test/java/com/example/demo/repository/UserRepositoryTest.java
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -15,41 +15,16 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest(showSql = true)
 @TestPropertySource("classpath:test-application.properties")
+@Sql("/sql/user-repository-test-data.sql")
 public class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
     @Test
-    void UserRepository가_제대로_연결되었다() {
-        // Given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setEmail("test@gmail.com");
-        userEntity.setAddress("Seoul");
-        userEntity.setNickname("thomas");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaaaa-aaaaa-aaaa-aaa-aaaaa");
-
-        // When
-        UserEntity res = userRepository.save(userEntity);
-    	
-        // Then
-        assertThat(res.getId()).isNotNull();
-    }
-
-    @Test
     void findByIdAndStatus로_유저_데이터를_찾아올_수_있다() {
         // Given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("test@gmail.com");
-        userEntity.setAddress("Seoul");
-        userEntity.setNickname("thomas");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaaaa-aaaaa-aaaa-aaa-aaaaa");
-
         // When
-        userRepository.save(userEntity);
         Optional<UserEntity> res = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
 
         // Then
@@ -59,16 +34,7 @@ public class UserRepositoryTest {
     @Test
     void findByIdAndStatus로_유저_데이터가_없으면_Optional_empty_를_내려준다() {
         // Given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("test@gmail.com");
-        userEntity.setAddress("Seoul");
-        userEntity.setNickname("thomas");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaaaa-aaaaa-aaaa-aaa-aaaaa");
-
         // When
-        userRepository.save(userEntity);
         Optional<UserEntity> res = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
 
         // Then
@@ -78,16 +44,7 @@ public class UserRepositoryTest {
     @Test
     void findByEmailAndStatus로_유저_데이터를_찾아올_수_있다() {
         // Given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("test@gmail.com");
-        userEntity.setAddress("Seoul");
-        userEntity.setNickname("thomas");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaaaa-aaaaa-aaaa-aaa-aaaaa");
-
         // When
-        userRepository.save(userEntity);
         Optional<UserEntity> res = userRepository.findByEmailAndStatus("test@gmail.com", UserStatus.ACTIVE);
 
         // Then
@@ -97,16 +54,7 @@ public class UserRepositoryTest {
     @Test
     void findByEmailAndStatus로_유저_데이터가_없으면_Optional_empty_를_내려준다() {
         // Given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("test@gmail.com");
-        userEntity.setAddress("Seoul");
-        userEntity.setNickname("thomas");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaaaa-aaaaa-aaaa-aaa-aaaaa");
-
         // When
-        userRepository.save(userEntity);
         Optional<UserEntity> res = userRepository.findByEmailAndStatus("test@gmail.com", UserStatus.PENDING);
 
         // Then
---
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+insert into `users` (`id`,`email`,`nickname`,`address`,`certification_code`,`status`,`last_login_at`)
+values (1,'test@gmail.com','thomas','Seoul','aaaaaaa-aaaaa-aaaa-aaa-aaaaa','ACTIVE',0);
