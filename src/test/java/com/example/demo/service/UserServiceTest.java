4c9dde4dbe1693aac8f065d2a1cdd02da7664faa
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-08-04 12:28:05 +0900

    :zap: userServuce read 메소드에 테스트 코드 추가

--- src/test/java/com/example/demo/service/UserServiceTest.java
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -8,12 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @SpringBootTest
 @TestPropertySource("classpath:test-application.properties")
-@Sql("/sql/user-service-test-data.sql")
+// sql 파일을 여러 개 지정해서 실행가능
+@SqlGroup({
+        @Sql(value = "/sql/user-service-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
 public class UserServiceTest {
     @Autowired
     private UserService userService;
@@ -40,6 +46,25 @@ public class UserServiceTest {
         assertThatThrownBy(() -> {
             userService.getByEmail(email);
         }).isInstanceOf(ResourceNotFoundException.class);
+    }
 
+    @Test
+    void getById은_ACTIVE_상태인_유저를_찾아올_수_있다() {
+        // Given
+        // When
+        UserEntity res = userService.getById(1);
+
+        // Then
+        assertThat(res.getNickname()).isEqualTo("thomas");
+    }
+
+    @Test
+    void getById은_PENDING_상태인_유저를_찾아올_수_없다() {
+        // Given
+        // When
+        // Then
+        assertThatThrownBy(() -> {
+            userService.getById(2);
+        }).isInstanceOf(ResourceNotFoundException.class);
     }
 }
